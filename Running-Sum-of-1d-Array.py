39a854e229da75f91a01c9fd104335e7a9c10214
Hamritha V <hamrithav3@gmail.com>
2025-07-28 09:06:15 +0530

    [1480] [Time Beats: 100.00%] [Memory Beats: 42.14%] - LeetPush

---
+++ Running-Sum-of-1d-Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        arr = []
+        arr.append(nums[0])
+
+        for i in range(1, n):
+            x = arr[i-1] + nums[i]
+            arr.append(x)
+
+        return arr
